bd5ce6581f18e0e0fea6253fb87335b28e7e29f2
shilpa <shilpa@nextolive.com>
2019-04-20 20:14:21 +0530

    filter

--- src/pages/filter/filter.ts
+++ src/pages/filter/filter.ts
@@ -22,14 +22,14 @@ export class FilterPage {
 	myDate2: any;
 	clear: boolean = false;
 	dateFilter = {};
-	Status : Array<{ id: number, pin_status_name: string,color_code:string, isChecked : false }> = [];
-	assignee : Array<{ id: number, full_name: string,color:string, isChecked: false }> = [];
-	data_filter:any=[];	
-	date:Array<{ from: string, to: string, isChecked: boolean, day:string }> = [];
-	id:number=0;
+	Status: Array<{ id: number, pin_status_name: string, color_code: string, isChecked: false }> = [];
+	assignee: Array<{ id: number, full_name: string, color: string, isChecked: false }> = [];
+	data_filter: any = [];
+	date: Array<{ from: string, to: string, isChecked: boolean, day: string }> = [];
+	id: number = 0;
 	constructor(public navCtrl: NavController, public navParams: NavParams, private datePicker: DatePicker, private calendar: Calendar, public filterService: FilterProvider) {
 		this.getUserList();
-		this.getStatus();		
+		this.getStatus();
 		this.dateFilter = {
 			today: false,
 			yesterday: false,
@@ -39,7 +39,7 @@ export class FilterPage {
 			lastMonth: false,
 			thisYear: false,
 			lastYear: false,
-			custom:false
+			custom: false
 		};
 	}
 	todo = {};
@@ -53,6 +53,8 @@ export class FilterPage {
 		let data = this.dateFilter;
 		Object.keys(data).forEach(key => {
 			this.dateFilter[key] = false;
+			this.date=[];
+			this.data_filter=[];
 		});
 		let data1 = this.Status;
 		Object.keys(data1).forEach(key => {
@@ -67,7 +69,7 @@ export class FilterPage {
 	LoginRemCheck(data) {
 		let i = 0;
 		Object.keys(data).forEach(key => {
-			if (data[key] == true) {			
+			if (data[key] == true) {
 				i++;
 			}
 		});
@@ -79,23 +81,24 @@ export class FilterPage {
 	DateCheck(data) {
 		let i = 0;
 		Object.keys(data).forEach(key => {
-			if (data[key] == true) {			
+			if (data[key] == true) {
 				i++;
-				this.DateConvert(key,true);
+				this.DateConvert(key, true);
 			}
-			else{
-				this.DateConvert(key,false);
+			else {
+				this.clear = true;
+				//this.DateConvert(key, false);
 			}
 		});
 		if (i > 0) {
 			this.clear = true;
 		}
 	}
-	changeFilter(val) {		
+	changeFilter(val) {
 		this.filter = val;
 	}
 	goToMap() {
-		this.data_filter=[];
+		this.data_filter = [];
 		for (var key in this.Status) {
 			if (this.Status[key].isChecked) {
 				if (this.data_filter['status'] != null || this.data_filter['status'] != undefined) {
@@ -118,13 +121,16 @@ export class FilterPage {
 		}
 		for (var key in this.date) {
 			if (this.date[key].isChecked) {
-				if(key!="today" && key!="yesterday"&&key!="custom"){
-				this.data_filter['start_date'] = this.date[key].from;
-				this.data_filter['end_date'] = this.date[key].to;
-			}
-			else{
-				this.data_filter['custom_date'] = this.date[key].to;
-			}
+				if (key != "today" && key != "yesterday" ) {
+					this.data_filter['start_date'] = this.date[key].from;
+					this.data_filter['end_date'] = this.date[key].to;
+				}
+			/*	else if(key != "custom"){
+					this.data_filter['start_date']=document.getElementsByClassName[0]('datetime-text').innerText;
+				}*/
+				else {
+					this.data_filter['custom_date'] = this.date[key].to;
+				}
 			}
 		}
 		this.navCtrl.setRoot('MapPage', { filter: this.data_filter });
@@ -146,130 +152,131 @@ export class FilterPage {
 			}
 		});
 	}
- getFirstLastDayOfLastWeek(userDate) {
-debugger;
-  let result = {};
+	getFirstLastDayOfLastWeek(userDate) {
+		debugger;
+		let result = {};
 
-  let curr = new Date(userDate); // get current date
-  let last = (curr.getDate()-1) - curr.getDay(); // First day is the day of the month - the day of the week
-  let first = last - 6; // last day is the first day + 6
-var firstDay = new Date(curr.setDate(first));
-var lastDay = new Date(curr.setDate(last));
-  result = {
-    firstDay:firstDay.getDate+'-'+firstDay.getMonth+'-'+firstDay.getFullYear,
-    lastDay:lastDay.getDate+'-'+lastDay.getMonth+'-'+lastDay.getFullYear
-  };
+		let curr = new Date(userDate); // get current date
+		let last = (curr.getDate() - 1) - curr.getDay(); // First day is the day of the month - the day of the week
+		let first = last - 6; // last day is the first day + 6
+		var firstDay = new Date(curr.setDate(first));
+		var lastDay = new Date(curr.setDate(last));
+		result = {
+			firstDay: firstDay.getDate + '-' + firstDay.getMonth + '-' + firstDay.getFullYear,
+			lastDay: lastDay.getDate + '-' + lastDay.getMonth + '-' + lastDay.getFullYear
+		};
 
-  return result;
-};
-getFirstLastDayOfthisWeek(userDate) {
-debugger;
-  let result = {};
+		return result;
+	};
+	getFirstLastDayOfthisWeek(userDate) {
+		debugger;
+		let result = {};
 
-  let curr = new Date(userDate); // get current date
-  let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
-  let last = first + 6; // last day is the first day + 6
-var firstDay = new Date(curr.setDate(first));
-var lastDay = new Date(curr.setDate(last));
-  result = {
-       firstDay:firstDay.getDate+'-'+firstDay.getMonth+'-'+firstDay.getFullYear,
-    lastDay:lastDay.getDate+'-'+lastDay.getMonth+'-'+lastDay.getFullYear
-  };
+		let curr = new Date(userDate); // get current date
+		let first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+		let last = first + 6; // last day is the first day + 6
+		var firstDay = new Date(curr.setDate(first));
+		var lastDay = new Date(curr.setDate(last));
+		result = {
+			firstDay: firstDay.getDate + '-' + firstDay.getMonth + '-' + firstDay.getFullYear,
+			lastDay: lastDay.getDate + '-' + lastDay.getMonth + '-' + lastDay.getFullYear
+		};
 
-  return result;
-};
-getFirstLastDayOfthisMonth() {
-debugger;
-  let result = {};
-var date = new Date(), y = date.getFullYear(), m = date.getMonth();
-var firstDay = new Date(y, m, 1);
-var lastDay = new Date(y, m + 1, 0);
+		return result;
+	};
+	getFirstLastDayOfthisMonth() {
+		debugger;
+		let result = {};
+		var date = new Date(), y = date.getFullYear(), m = date.getMonth();
+		var firstDay = new Date(y, m, 1);
+		var lastDay = new Date(y, m + 1, 0);
 
-  result = {
-        firstDay:firstDay.getDate+'-'+firstDay.getMonth+'-'+firstDay.getFullYear,
-    lastDay:lastDay.getDate+'-'+lastDay.getMonth+'-'+lastDay.getFullYear
-  };
+		result = {
+			firstDay: firstDay.getDate + '-' + firstDay.getMonth + '-' + firstDay.getFullYear,
+			lastDay: lastDay.getDate + '-' + lastDay.getMonth + '-' + lastDay.getFullYear
+		};
 
-  return result;
-};
-getFirstLastDayOflastMonth() {
-debugger;
-  let result = {};
-var date = new Date(), y = date.getFullYear(), m = date.getMonth()-1;
-var firstDay = new Date(y, m, 1);
-var lastDay = new Date(y, m + 1, 0);
+		return result;
+	};
+	getFirstLastDayOflastMonth() {
+		debugger;
+		let result = {};
+		var date = new Date(), y = date.getFullYear(), m = date.getMonth() - 1;
+		var firstDay = new Date(y, m, 1);
+		var lastDay = new Date(y, m + 1, 0);
 
-  result = {
-         firstDay:firstDay.getDate+'-'+firstDay.getMonth+'-'+firstDay.getFullYear,
-    lastDay:lastDay.getDate+'-'+lastDay.getMonth+'-'+lastDay.getFullYear
-  };
-  return result;
-};
-getFirstLastDayOfthisYear() {
-debugger;
-  let result = {};
-var firstDay = new Date(new Date().getFullYear(), 0, 1)
-var lastDay = new Date(new Date().getFullYear(), 11, 31)
+		result = {
+			firstDay: firstDay.getDate + '-' + firstDay.getMonth + '-' + firstDay.getFullYear,
+			lastDay: lastDay.getDate + '-' + lastDay.getMonth + '-' + lastDay.getFullYear
+		};
+		return result;
+	};
+	getFirstLastDayOfthisYear() {
+		debugger;
+		let result = {};
+		var firstDay = new Date(new Date().getFullYear(), 0, 1)
+		var lastDay = new Date(new Date().getFullYear(), 11, 31)
 
-  result = {
-        firstDay:firstDay.getDate+'-'+firstDay.getMonth+'-'+firstDay.getFullYear,
-    lastDay:lastDay.getDate+'-'+lastDay.getMonth+'-'+lastDay.getFullYear
-  };
-  return result;
-};
-getFirstLastDayOflastYear() {
-debugger;
-  let result = {};
-var firstDay = new Date(new Date().getFullYear()-1, 0, 1)
-var lastDay = new Date(new Date().getFullYear()-1, 11, 31)
+		result = {
+			firstDay: firstDay.getDate + '-' + firstDay.getMonth + '-' + firstDay.getFullYear,
+			lastDay: lastDay.getDate + '-' + lastDay.getMonth + '-' + lastDay.getFullYear
+		};
+		return result;
+	};
+	getFirstLastDayOflastYear() {
+		debugger;
+		let result = {};
+		var firstDay = new Date(new Date().getFullYear() - 1, 0, 1)
+		var lastDay = new Date(new Date().getFullYear() - 1, 11, 31)
 
-  result = {
-          firstDay:firstDay.getDate+'-'+firstDay.getMonth+'-'+firstDay.getFullYear,
-    lastDay:lastDay.getDate+'-'+lastDay.getMonth+'-'+lastDay.getFullYear
-  };
-  return result;
-};
+		result = {
+			firstDay: firstDay.getDate + '-' + firstDay.getMonth + '-' + firstDay.getFullYear,
+			lastDay: lastDay.getDate + '-' + lastDay.getMonth + '-' + lastDay.getFullYear
+		};
+		return result;
+	};
 	DateConvert(type: string, isChecked) {
+		this.date=[];
 		if (isChecked == true) {
 			switch (type) {
-				case "todaye":
-					this.date.push({ from: '', to: new Date().toString(), isChecked: true, day:type });
+				case "today":
+					this.date.push({ from: '', to: new Date().toString(), isChecked: true, day: type });
 					break;
 				case "yesterday":
-					this.date.push({ from: '', to: (new Date().getDate() - 1).toString(), isChecked: true, day:type });
+					this.date.push({ from: '', to: (new Date().getDate() - 1).toString(), isChecked: true, day: type });
 					break;
 				case "thisWeek": {
 					let dt = this.getFirstLastDayOfthisWeek(new Date().toString());
-					this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day:type });
+					this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day: type });
 					break;
 				}
 				case "lastWeek": {
 					let dt = this.getFirstLastDayOfLastWeek(new Date().toString());
-					this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day:type});
+					this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day: type });
 					break;
 				}
 				case "thisMonth":
 					{
 						let dt = this.getFirstLastDayOfthisMonth();
-						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day:type});
+						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day: type });
 						break;
 					}
 				case "lastMonth":
 					{
 						let dt = this.getFirstLastDayOflastMonth();
-						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day:type });
+						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day: type });
 						break;
 					}
 				case "thisYear":
 					{
 						let dt = this.getFirstLastDayOfthisYear();
-						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day:type });
+						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day: type });
 						break;
 					}
 				case "lastYear":
 					{
 						let dt = this.getFirstLastDayOflastYear();
-						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day:type });
+						this.date.push({ from: dt['firstday'], to: dt['lastday'], isChecked: true, day: type });
 						break;
 					}
 				default:
@@ -277,13 +284,13 @@ var lastDay = new Date(new Date().getFullYear()-1, 11, 31)
 					break;
 			}
 		}
-		else{
+		/*else {
 			debugger;
-			this.date.forEach(function(value){
-				if(value.day==type){
-					this.date.splice(value,1);
+			this.date.forEach(function(value) {
+				if (value.day == type) {
+					this.date.splice(value, 1);
 				}
 			})
-		}
+		}*/
 	}
 }
